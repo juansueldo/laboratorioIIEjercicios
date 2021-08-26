b15098cdc11a7e5015f2533282e887bcf0a0a3ce
unknown <juan.sueldo1989@gmail.com>
2021-08-26 20:32:22 -0300

    Ejercicio nro 9 y 11

---
+++ repos/EjercicioNro11/EjercicioNro11/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNro11
+{
+    class Class1
+    {
+        static public bool Validar(int valor, int min, int max)
+        {
+            bool retorno;
+            if(valor >= min && valor <= max)
+            {
+                retorno = true;
+            }
+            else
+            {
+                retorno = false;
+            }
+            return retorno;
+        }
+    }
+}
